e05c094570a38c83392e216962bea1f0e49eba34
unknown <ppetrov@WSKARLAPTOP03.corp.net>
2014-06-12 11:11:52 +0200

    Add comments

--- test03.js
+++ test03.js
@@ -3,7 +3,11 @@
  */
 var fs = require('fs');
 
-var person = function(name) {
+/*
+  New implicitly created object that uses
+  private vars to initialize its properties
+*/
+var Person = function(name) {
   var vorname = name || 'Default';
 
   return {
@@ -14,8 +18,44 @@ var person = function(name) {
   };
 };
 
-var p1 = person();
+/*
+  New object by copying only a set
+  of required properties from parent
+*/
+var Employee = function(name) {
+  var p = Person(name);
+  return {
+    'vorname': p.vorname,
+    name: function() {
+      return 'There isn\'t any!';
+    }
+  };
+};
+
+/*
+  New object by copying all public properties
+  from parent object
+*/
+var Customer = function(name) {
+  var obj = {}; // new empty object
+  var p = Person(name);
+  for(var i in p) {
+    // console.log(i);
+    obj[i] = p[i];
+  }
+  return obj;
+};
+
+var p1 = Person();
 console.log(p1, p1.name());
 
-var p2 = person('Texas');
+var p2 = Person('Texas');
 console.log(p2, p2.name());
+
+var e1 = Employee('Elon');
+console.log(e1, e1.name());
+
+var c1 = Customer('Customer');
+console.log(c1, c1.name());
+
+
